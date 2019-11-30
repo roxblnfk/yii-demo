960757e3b4e7893a8bf1eac64e0fbd37f2fdafac
roxblnfk <roxblnfk@ya.ru>
2019-11-30 17:12:59 +0300

    typo fixes

--- psr-worker.php
+++ psr-worker.php
@@ -42,11 +42,11 @@ while ($request = $psr7->acceptRequest()) {
 
         $response = new \Nyholm\Psr7\Response();
         $response->getBody()->write("Steps: {$steps};"
-            . "\n  T1: {$s1t} (Objects + Autowiring)"
+            . "\n  T1: {$s1t} (OOP Configs + Autowiring)"
             . "\n  T2: {$s2t} (Arrays)"
-            . "\n  T3: {$s3t} (Object + Factory)"
-            . "\nDelta(T1-T2): " . round(1000 * ($s1t - $s2t), 4) . "ms"
-            . "\nDelta(T3-T2): " . round(1000 * ($s3t - $s2t), 4) . "ms"
+            . "\n  T3: {$s3t} (OOP Configs + Factory)"
+            . "\nDelta(T1-T2): " . round(1000 * ($s1t - $s2t), 3) . " ms"
+            . "\nDelta(T3-T2): " . round(1000 * ($s3t - $s2t), 3) . " ms"
             . "\nMultiplier(T1-T2): x" . round($s1t / $s2t, 2)
             . "\nMultiplier(T3-T2): x" . round($s3t / $s2t, 2)
             . "\nAvg overhead(T1-T2): " . round(1000 * ($s1t - $s2t) / $steps, 4) . ' ms'
